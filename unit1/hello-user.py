a41f9c375ff33c224737557bf5a29e9a46af0914
infiniteLoop-zZz <philrose81@sky.com>
2024-10-01 22:22:56 +0100

    Added unit1

---
+++ unit1/hello-user.py
@@ -0,0 +1,23 @@
+#task 1.6
+# name = input("Hello, what is your name?\n")
+# age = input("What is your age?\n")
+# print(f"Hello {name}, your age is {age}.")
+
+# exercises 1.7
+
+# input_string = input("Please enter something:\n")
+# print(f"You entered - {input_string}")
+
+# input_string = input("Please enter a number:\n")
+# x = int(input_string)
+# print(f"{x} + 5 = {x+5}")
+
+# # above code on one line
+# x = int(input("Please enter a number:\n"))
+# print(f"{x} + 5 = {x+5}")
+
+# task 1.7
+
+num = int(input("Please enter a number:\n"))
+print(f"Your number multiplied by 10 equals: {num * 10}")
+
---
+++ unit1/hello-world.py
@@ -0,0 +1,61 @@
+print("Hello World")
+
+# task 1.1
+
+print("Hello World! is cool!")
+
+# task 1.2
+
+# This is a single line comment in Python
+
+print("Single line comments start with #")
+
+# print("Hello")
+# print("World")
+# print("with Comments")
+
+#task 1.3
+
+x = 12 + 5
+print(x)
+print(type(x))
+
+x = 5 + 3.0
+print(x)
+print(type(x))
+
+x = 12 / 5
+print(x)
+print(type(x))
+
+x = 12 + 5 == 5 * 3.0
+print(x)
+print(type(x))
+
+#task 1.4
+
+# You need to fix the following lines
+# Run the code and then use the error messages to fix each line
+
+# This line has a SyntaxError
+print("hello world")
+
+# This line has a NameError
+print("Hello World!")
+
+# The following lines cause a TypeError
+int1 = 100
+int2 = 10
+print(int1 / int2)
+
+#task 1.5
+
+# This line is not indented properly. Fix this
+print("Indentation matters in Python")
+
+# You will notice that we have blank lines
+# Python doesn't care about these!
+
+# There is extra whitespace inside the print statement 
+# Python doesn't care about this
+print("Hello World!")
